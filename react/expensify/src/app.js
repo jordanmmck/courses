5f0b73b7a6ef0e1e34d6d2f6c0f20209bf276f6d
Jordan McKinney <jordanmmck@gmail.com>
2019-09-11 15:55:20 -0700

    prod optimizations

--- react/expensify/src/app.js
+++ react/expensify/src/app.js
@@ -3,23 +3,12 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
-import { addExpense } from './actions/expenses';
-import { setTextFilter } from './actions/filters';
-import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
 import 'react-dates/initialize';
 
 const store = configureStore();
-
-store.dispatch(addExpense({ description: 'water bill', amount: 10000, createdAt: 555 }));
-store.dispatch(addExpense({ description: 'gas bill', amount: 5000, createdAt: 799 }));
-store.dispatch(addExpense({ description: 'rent bill', amount: 150000, createdAt: 1599 }));
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.info(visibleExpenses);
 
 const jsx = (
   <Provider store={store}>
--- react/expensify/webpack.config.js
+++ react/expensify/webpack.config.js
@@ -1,38 +1,50 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-module.exports = {
-  entry: './src/app.js',
-  mode: 'development',
-  output: {
-    path: path.resolve(__dirname, 'public'),
-    filename: 'bundle.js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        use: { loader: 'babel-loader' },
-        exclude: /node_modules/,
-      },
-      {
-        test: /\s?.css$/,
-        use: [
-          {
-            loader: 'style-loader',
-          },
-          {
-            loader: 'css-loader',
-          },
-          {
-            loader: 'sass-loader',
-          },
-        ],
-      },
-    ],
-  },
-  devtool: 'cheap-module-eval-source-map',
-  devServer: {
-    contentBase: path.resolve(__dirname, 'public'),
-    historyApiFallback: true,
-  },
+module.exports = env => {
+  const isProd = env === 'production';
+  const CSSExtract = new ExtractTextPlugin('styles.css');
+
+  return {
+    entry: './src/app.js',
+    mode: isProd ? 'production' : 'development',
+    output: {
+      path: path.resolve(__dirname, 'public'),
+      filename: 'bundle.js',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          use: { loader: 'babel-loader' },
+          exclude: /node_modules/,
+        },
+        {
+          test: /\s?.css$/,
+          use: CSSExtract.extract({
+            use: [
+              {
+                loader: 'css-loader',
+                options: {
+                  sourceMap: true,
+                },
+              },
+              {
+                loader: 'sass-loader',
+                options: {
+                  sourceMap: true,
+                },
+              },
+            ],
+          }),
+        },
+      ],
+    },
+    plugins: [CSSExtract],
+    devtool: isProd ? 'source-map' : 'inline-source-map',
+    devServer: {
+      contentBase: path.resolve(__dirname, 'public'),
+      historyApiFallback: true,
+    },
+  };
 };
