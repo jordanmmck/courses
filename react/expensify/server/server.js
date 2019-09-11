0e311fe4c1ef7b938130f746cc95075beb6bc6a8
Jordan McKinney <jordanmmck@gmail.com>
2019-09-11 16:30:13 -0700

    prod server

---
+++ react/expensify/server/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const publicPath = path.join(__dirname, '..', 'public');
+
+app.use(express.static(publicPath));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(publicPath, 'index.html'));
+});
+
+app.listen(3000, () => {
+  console.info('running on 3000...');
+});
