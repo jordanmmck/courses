e9caa06caf22740e13dcd607671b221c5a991218
Jordan McKinney <jordanmmck@gmail.com>
2019-12-13 15:59:34 -0800

    flutter

---
+++ flutter/i_am_rich/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+      MaterialApp(
+        home: Scaffold(
+            backgroundColor: Colors.blueGrey,
+            appBar: AppBar(
+              title: Text('Aviary.\$'),
+              backgroundColor: Colors.blueGrey[900],
+            ),
+            body: Center(
+              child: Image(
+                image: AssetImage('images/diamond.png'),
+              ),
+            )),
+      ),
+    );
